730dd3b70f4c792020dfda1135420aa081c780dd
NanduKrishnaDev <nndukrishna1998@gmail.com>
2020-12-28 13:28:37 +0530

    2nd commit

--- main.js
+++ main.js
@@ -20,15 +20,11 @@ function dropdown(){ //to activate the drop down
     document.getElementById("drop").style.display="block";
 }
 
-function modal(){  //to activate the modal
+function modal(){  //to activate the modal1
     document.getElementById("modal").style.display="block";
 }
 
-
-// document.getElementById("modalclose").onclick=function(e){document.getElementById("modal").style.display="none";}
-
-
-document.getElementById("not1").addEventListener("click",(e)=>{
+document.getElementById("not1").addEventListener("click",(e)=>{ //to activate audio1 on click
     var audio= document.getElementsByTagName("audio")[0];
     audio.play();
 })
@@ -51,6 +47,32 @@ function sidebar(){ // to activate sidebar
     //  document.getElementById("sidebar").style.display="block";
 }
 
+function change(clicked){ //to make logos active
+     for(let i=0;i<6;i++){ document.getElementsByClassName("logopart")[i].style.color="#e20074";}
+     document.getElementsByClassName("logopart")[clicked].style.color="#fc6e28";
+ }
+
+function colorchange1(clicked){ //to activate color in mouse over
+    document.getElementsByClassName(clicked)[0].style.backgroundColor="#f5f5f5";
+    document.getElementsByClassName(clicked)[0].style.borderColor="#dddddd";
+    
+ }
+
+function colorchange2(clicked){  //to inactivate color in mouse over by mouse out
+    document.getElementsByClassName(clicked)[0].style.backgroundColor="#ffffff";
+    document.getElementsByClassName(clicked)[0].style.borderColor="#cccccc";
+    
+ }
+function mod(clicked){  //to activate modal2
+    let n =parseInt(clicked[3]);
+    let m=n+1;
+    let identity = 'row' + m;
+   let t= document.getElementsByClassName(identity)[0].getElementsByClassName('column2')[0].innerHTML;
+   document.getElementById("tex").innerHTML=t;
+   document.getElementById("modl").style.display="block";
+   
+}
+
 window.onload = function(){   // to close all popups on clicking the window
     document.onclick = function(e){
        if((e.target.id !== 'modal1')&&(e.target.id !== 'image3')){
@@ -68,30 +90,3 @@ window.onload = function(){   // to close all popups on clicking the window
 
     };
  };
-
-
- function change(clicked){
-     for(let i=0;i<6;i++){ document.getElementsByClassName("logopart")[i].style.color="#e20074";}
-     document.getElementsByClassName("logopart")[clicked].style.color="#fc6e28";
- }
-
- function colorchange1(clicked){
-    document.getElementsByClassName(clicked)[0].style.backgroundColor="#f5f5f5";
-    document.getElementsByClassName(clicked)[0].style.borderColor="#dddddd";
-    
- }
-
- function colorchange2(clicked){
-    document.getElementsByClassName(clicked)[0].style.backgroundColor="#ffffff";
-    document.getElementsByClassName(clicked)[0].style.borderColor="#cccccc";
-    
- }
-function mod(clicked){
-    let n =parseInt(clicked[3]);
-    let m=n+1;
-    let identity = 'row' + m;
-   let t= document.getElementsByClassName(identity)[0].getElementsByClassName('column2')[0].innerHTML;
-   document.getElementById("tex").innerHTML=t;
-   document.getElementById("modl").style.display="block";
-   
-}
